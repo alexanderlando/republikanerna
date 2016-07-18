037d9279713bd8bb22c173d3880610df1bb5c402
Alexander Fairshore <fairshore@Alexanders-MacBook-Pro.local>
2016-07-18 18:15:51 +0200

    Initial commit of my single page repo

--- index.php
+++ index.php
@@ -7,5 +7,5 @@
 <img src="republikanerna.png"><br><br>
 <iframe width="800" height="480" src="https://www.youtube.com/embed/nwbYLlq7iYo" frameborder="0" allowfullscreen></iframe>
 </body><br>
-<font face="verdana, arial" size="2" color="#FFFFFF"><a href="mailto:alexander@republikanerna.com">Kontakta Republikanerna</a></font>
+<font face="verdana, arial" size="2" color="#FFFFFF"><a href="mailto:alexander@republikanerna.com" style="color:white">Kontakta Republikanerna</a></font>
 </html>
