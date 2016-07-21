af6c9b9bab4ee56da2789990b18825ed9315dae0
Alexander Fairshore <fairshore@Alexanders-MacBook-Pro.local>
2016-07-21 02:04:04 +0200

    Initial commit of my single page repo

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <meta property="og:description" content="Livestreama Republikanernas Konvent 2016 på Svenska Republikanerna.com" />
 </head>
 <body bgcolor="#0052A5">
+<img src="virubo.png" style="float: left;" />
 <center>
 <img src="republikanerna90.png"><br>
 <iframe width="800" height="480" src="https://www.youtube.com/embed/r_lpyG-ddec?list=PL_lJl7Q_lhSbmZSnnAUFzx4K-GPTZIgUU" frameborder="0" allowfullscreen></iframe>
